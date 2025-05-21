3c00faf5e5a2ce61dbd594827ed1eb572186155a
Neda <mnimid@.com>
2025-05-21 08:43:56 +0300

    +eng

--- src/test/java/MessageSenderTest.java
+++ src/test/java/MessageSenderTest.java
@@ -1,4 +1,3 @@
-import jdk.javadoc.doclet.Taglet;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
@@ -10,7 +9,6 @@ import ru.netology.sender.MessageSenderImpl;
 import java.util.Map;
 import java.util.TreeMap;
 
-import static org.mockito.ArgumentMatchers.any;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static ru.netology.entity.Country.RUSSIA;
@@ -29,4 +27,18 @@ public class MessageSenderTest {
         testmap.put("x-real-ip","172.0.32.11");
         messageSender.send(testmap);
     }
+    
+    @Test
+    void send_test_eng(){
+        GeoServiceImpl geoService= Mockito.mock(GeoServiceImpl.class);
+        Mockito.when(geoService.byIp(anyString()))
+                .thenReturn(new Location("New York", Country.USA, " 10th Avenue", 32));
+        LocalizationServiceImpl localizationService=Mockito.mock(LocalizationServiceImpl.class);
+        
+        MessageSenderImpl messageSender=new MessageSenderImpl(geoService,localizationService);
+        Map<String,String> testmap=new TreeMap<>();
+        testmap.put("x-real-ip","96.44.183.149");
+        messageSender.send(testmap);
+    }
+
 }
